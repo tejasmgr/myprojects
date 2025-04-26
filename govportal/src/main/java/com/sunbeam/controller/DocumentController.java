fbed49af5000e7e801061f88e242af3e9d4d04eb
Tejas Magar <magartejas2001@gmail.com>
2025-04-26 21:08:31 +0530

    DocumentController layer Service Layer created

--- govportal/src/main/java/com/sunbeam/controller/DocumentController.java
+++ govportal/src/main/java/com/sunbeam/controller/DocumentController.java
@@ -3,8 +3,12 @@ package com.sunbeam.controller;
 import com.sunbeam.dto.request.DocumentApplicationRequest;
 import com.sunbeam.dto.response.DocumentApplicationResponse;
 import com.sunbeam.service.DocumentService;
+import com.sunbeam.service.VerificationWorkflowService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +20,14 @@ import java.util.List;
 public class DocumentController {
 
     private final DocumentService documentService;
+    private final VerificationWorkflowService workflowService;
 
-    @PostMapping
+    @PostMapping("/ submit-application")
     public ResponseEntity<DocumentApplicationResponse> submitApplication(
             @Valid @ModelAttribute DocumentApplicationRequest request) {
-        return ResponseEntity.ok(documentService.submitApplication(request));
+    	DocumentApplicationResponse response = documentService.submitApplication(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+       
     }
 
     @GetMapping("/my-applications")
