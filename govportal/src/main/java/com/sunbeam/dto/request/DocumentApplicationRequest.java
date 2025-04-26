2c8da3ba312a0fb786b9c0015a4581a7aedfba78
Tejas Magar <magartejas2001@gmail.com>
2025-04-26 21:10:16 +0530

    DocumentController along with RequestDTO created

--- govportal/src/main/java/com/sunbeam/dto/request/DocumentApplicationRequest.java
+++ govportal/src/main/java/com/sunbeam/dto/request/DocumentApplicationRequest.java
@@ -1,17 +1,30 @@
 package com.sunbeam.dto.request;
 import com.sunbeam.model.DocumentApplication.DocumentType;
+
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @Data
 public class DocumentApplicationRequest {
-    @NotNull(message = "Document type is required")
+	@NotNull(message = "Document type is required")
     private DocumentType documentType;
     
+	private String fullName;
+	
+	private String address;
+	
     @NotNull(message = "Documents are required")
+    @Size(min = 1, message = "At least one document must be uploaded")
     private List<MultipartFile> documents;
     
+    @NotBlank(message = "Purpose is required")
+    @Size(max = 500, message = "Purpose must be less than 500 characters")
     private String purpose;
+    
+    // Additional fields if needed
+//    private String additionalInfo;
 }
