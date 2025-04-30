e1176561ca44de2cdf452d724f6a2d6e2029f735
tejasmgr2001 <magartejas.scoe.civil@gmail.com>
2025-04-30 21:17:04 +0530

    swagger issue Resolved

--- govportal/src/main/java/com/sunbeam/config/SwaggerConfig.java
+++ govportal/src/main/java/com/sunbeam/config/SwaggerConfig.java
@@ -1,22 +1,20 @@
-package com.sunbeam.config;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
-}
+//package com.sunbeam.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//@Configuration
+//public class SwaggerConfig {
+//@Bean
+//public Docket api() {
+//return new Docket(DocumentationType.SWAGGER_2)
+//.select()
+//.apis(RequestHandlerSelectors.any())
+//.paths(PathSelectors.any())
+//.build();
+//}
+//}
