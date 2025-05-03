4a4fcf4a240b2968fe2f2266d0c84561d8d5f0f5
tejasmgr2001 <magartejas.scoe.civil@gmail.com>
2025-05-03 17:56:09 +0530

    Verification Service created

--- govportal/src/main/java/com/sunbeam/service/VerificationService.java
+++ govportal/src/main/java/com/sunbeam/service/VerificationService.java
@@ -1,17 +1,28 @@
 package com.sunbeam.service;
 
+import com.sunbeam.dto.response.DocumentApplicationDetailsResponse;
 import com.sunbeam.dto.response.DocumentApplicationResponse;
 import com.sunbeam.dto.response.VerificationStatsResponse;
 import com.sunbeam.model.DocumentApplication;
+
+
+import java.util.List;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 public interface VerificationService {
-    DocumentApplicationResponse assignToVerifier(Long applicationId, Long verifierId);
+	
+	ResponseEntity<DocumentApplicationResponse> getDocumentApplicationById(Long id);
     DocumentApplicationResponse approveApplication(Long applicationId, String remarks);
     DocumentApplicationResponse rejectApplication(Long applicationId, String remarks);
     DocumentApplicationResponse requestChanges(Long applicationId, String remarks);
-   // Page<DocumentApplicationResponse> getAssignedApplications(int page, int size);
- //   VerificationStatsResponse getVerificationStats();
+//    Page<DocumentApplicationResponse> getAssignedApplications(int page, int size);
+    VerificationStatsResponse getVerificationStats();
 //    DocumentApplicationResponse escalateToSeniorVerifier(Long applicationId, String reason);
+    Page<DocumentApplicationResponse>  getPendingApplications(Pageable pageable);
+	
+	
     
 }
