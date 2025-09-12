264a39aacf64aea7e1cfd940f6898b91ffecad71
Tejas Magar <magartejas2001@gmail.com>
2025-09-12 23:42:36 +0530

    unwanted code reduced

--- govportal/backend/src/main/java/com/sunbeam/repository/SystemConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sunbeam.repository;
-
-import com.sunbeam.model.SystemConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface SystemConfigRepository extends JpaRepository<SystemConfig, String> {
-    Optional<SystemConfig> findByConfigKey(String key);
-}
